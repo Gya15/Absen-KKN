39b65e2fb0e3b94ccae38e7ffda702042070fe1d
Gya15 <argyasatya72@gmail.com>
2026-07-20 06:29:58 +0700

    Use PG* variables natively to bypass parse_url special character failures on Railway

--- backend/config/database.php
+++ backend/config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DATABASE_URL') ? 'pgsql' : env('DB_CONNECTION', 'sqlite'),
+    'default' => env('PGHOST') ? 'pgsql' : (env('DATABASE_URL') ? 'pgsql' : env('DB_CONNECTION', 'sqlite')),
 
     /*
     |--------------------------------------------------------------------------
@@ -86,12 +86,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL', env('DB_URL')),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            // Disable URL parsing to avoid special character issues in password
+            'host' => env('PGHOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('PGPORT', env('DB_PORT', '5432')),
+            'database' => env('PGDATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('PGUSER', env('DB_USERNAME', 'root')),
+            'password' => env('PGPASSWORD', env('DB_PASSWORD', '')),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
