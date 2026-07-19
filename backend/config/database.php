d8f61e124c14cd64e1dc7ea65155cae2b2ff905c
Gya15 <argyasatya72@gmail.com>
2026-07-20 06:47:01 +0700

    Restore DATABASE_URL since config cache is now bypassed

--- backend/config/database.php
+++ backend/config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            // Disable URL parsing to avoid special character issues in password
+            'url' => env('DATABASE_URL'),
             'host' => env('PGHOST', env('DB_HOST', '127.0.0.1')),
             'port' => env('PGPORT', env('DB_PORT', '5432')),
             'database' => env('PGDATABASE', env('DB_DATABASE', 'laravel')),
