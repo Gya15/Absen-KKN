413ef4b4274b59a18adaed68354435b297386b2d
Gya15 <argyasatya72@gmail.com>
2026-07-20 05:26:57 +0700

    Fix CORS for Vercel deployment

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -25,7 +25,7 @@ return [
         'http://127.0.0.1:5173',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/https?:\/\/.*/'],
 
     'allowed_headers' => ['*'],
 
